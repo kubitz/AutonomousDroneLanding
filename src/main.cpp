e84ba45f636354394970e7c8b2ae54a48674e4eb
Noe Guisset <noe.guisset@gmail.com>
2021-05-13 13:06:21 +0100

    working simulation pipeline with saving lzs to files

--- src/main.cpp
+++ src/main.cpp
@@ -4,43 +4,58 @@
 #include "../include/labels.h"
 #include "../include/LzFinder.h"
 #include <filesystem>
+#include "algorithm"
 #include "../include/utils.h"
+
 namespace fs = std::filesystem;
-bool SIMULATE=true;
+bool SIMULATE = true;
 
 
 int main(int /*argc*/, char ** /*argv*/ ) {
-    std::cout << labels_graz[0] << std::endl;
     LzFinder lz_finder("test");
+    std::vector<fs::path> imgs_to_process;
+    auto resImg = utils::iterate_file("/home/kubitz/CLionProjects/FYPLanding/data/cfg/inputs.cfg",
+                                      [&](const std::string &str) {
+                                          imgs_to_process.emplace_back(fs::path(str));
+                                      });
     obstacle testObstacle{500, 500, 70, "human"};
     std::vector<obstacle> obstacles{testObstacle};
-
-    std::cout << "Current path is " << fs::current_path() << '\n'; // (1)
-    fs::current_path(fs::temp_directory_path()); // (3)
-    std::cout << "Current path is " << fs::current_path() << '\n';
-
-    std::vector<fs::path> imgs_to_process;
-    std::vector<fs::path> masks_to_process;
-    bool resImg = utils::iterate_file("/home/kubitz/CLionProjects/FYPLanding/data/cfg/inputs.cfg",
-                                      [&](const std::string &str) {
-                                          imgs_to_process.push_back(fs::path(str));
-                                      });
-    bool resMask = utils::iterate_file("/home/kubitz/CLionProjects/FYPLanding/data/cfg/masks.cfg",
-                                      [&](const std::string &str) {
-                                          imgs_to_process.push_back(fs::path(str));
-                                      });
-    if (resImg){
-        int i = 0;
-        i = 1;
+    landingZones lzs;
+    if (SIMULATE) {
+        std::vector<fs::path> masks_to_process;
+        auto resMask = utils::iterate_file("/home/kubitz/CLionProjects/FYPLanding/data/cfg/masks.cfg",
+                                           [&](const std::string &str) {
+                                               masks_to_process.emplace_back(fs::path(str));
+                                           });
+        for (auto i = 0; i < imgs_to_process.size(); i++) {
+            cv::Mat mask = cv::imread(std::string(masks_to_process[i]),cv::IMREAD_COLOR);
+            if (mask.empty()) {
+                std::cout << "Could not read the segmentation mask: " << std::string(masks_to_process[i]) << std::endl;
+                return 1;
+            }
+            std::string id = imgs_to_process[i].stem();
+            landingZones lzs_seq = lz_finder.get_ranked_lzs(mask, obstacles, 100,
+                                                            id, 100, 250);
+            std::move(lzs_seq.begin(), lzs_seq.end(), std::back_inserter(lzs));
+        }
+    } else {
+        // TODO: Add semantic + object detector engine
+        int i;
     }
+    fs::path path_results = utils::get_path_results(imgs_to_process[0]);
+    fs::create_directories(path_results);
+    utils::save_lzs(lzs, path_results / "results_lzs.csv");
 
-    cv::Mat img = cv::imread("/home/kubitz/CLionProjects/FYPLanding/data/imgs/0_simulation/images/041007_017.jpg", cv::IMREAD_COLOR);
-    cv::Mat seg_img = cv::imread("/home/kubitz/CLionProjects/FYPLanding/data/imgs/0_simulation/masks/041007_017_mask.jpg", cv::IMREAD_COLOR);
 
-    if (img.empty()) {
-        std::cout << "Could not read the image: " << std::endl;
-        return 1;
-    }
+    /*
+     * This section is just to test the individual functions - it will be removed in the final code
+     */
+    cv::Mat img = cv::imread("/home/kubitz/CLionProjects/FYPLanding/data/imgs/0_simulation/images/041007_017.jpg",
+                             cv::IMREAD_COLOR);
+    cv::Mat seg_img = cv::imread(
+            "/home/kubitz/CLionProjects/FYPLanding/data/imgs/0_simulation/masks/041007_017_mask.jpg", cv::IMREAD_COLOR);
+
+
     if (seg_img.empty()) {
         std::cout << "Could not read the image: " << std::endl;
         return 1;
@@ -49,9 +64,6 @@ int main(int /*argc*/, char ** /*argv*/ ) {
     cv::Mat annotated_img = lz_finder.draw_lzs(img, lzs_proposed, obstacles);
     cv::Mat risk_map = lz_finder.get_risk_map(seg_img);
     lz_finder.rank_lzs(lzs_proposed, risk_map);
-    fs::path test = utils::get_path_results(imgs_to_process[0]);
-    fs::create_directories(test);
-    utils::save_lzs(lzs_proposed,test / "lzs.csv");
     cv::applyColorMap(risk_map, risk_map, cv::COLORMAP_JET);
     cv::imshow("Display window", img);
     cv::imshow("risk_map", risk_map);
